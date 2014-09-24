0a141cbb50f249402423207eefa06b4fe872a50b
boris dev <boris.dev@gmail.com>
2014-09-24 09:54:17 -0700

    script to make a json of boundingboxes

---
+++ ETL/rtree/blocks_boundingbox.py
@@ -0,0 +1,31 @@
+
+
+import fiona
+import pysal
+import sys
+sys.path.append("/usr/local/lib/python2.7/site-packages")
+import utm
+import json
+
+shpfile="/Users/slow/workspace/geoscore/S3/shapefiles/USA_blockgroup_2010/US_blck_grp_2010.shp"
+shpfile="/Users/slow/workspace/splunk-choropleth/bin/usa_block_groups_lonlat_shapefile/usa_block_groups_lonlat.shp"
+
+# rbush expects
+# [minX, minY, maxX, maxY] (bounding box coordinates, or just [x, y, x, y]
+
+with open("bounding_boxes.json","w") as output:
+    bb_list=[]
+    shp=pysal.open(shpfile, 'r');
+    N=len(shp)
+    
+    for enum,i in enumerate(shp):
+        print enum,"of", N
+        x=i.bounding_box.left
+        y=i.bounding_box.lower
+        X=i.bounding_box.right
+        Y=i.bounding_box.upper
+        bb_list.append([x,y,X,Y])
+        #break
+    json.dump(bb_list,output)
+
+        
