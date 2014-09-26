0184d316572cc0b5e85474dacb54701906dc804d
boris dev <boris.dev@gmail.com>
2014-09-26 16:18:29 -0700

    saving node tree as json worked

--- ETL/rtree/bboxes_to_rtree.js
+++ ETL/rtree/bboxes_to_rtree.js
@@ -20,7 +20,7 @@ jsonFile(filename, function (err, file) {
         console.log("Inspect input data of bounding boxes:");
         console.log(typeof file.data);
         console.log(file.data.slice(0,2));
-        tree.load(file.data)
+        tree.load(file.data);
         var rec=[-117.70702999976885, 34.064940999658226, -117.69829200059435, 34.066827000259664];
         var result = tree.search(rec);
         var offsets=[];
@@ -32,20 +32,26 @@ jsonFile(filename, function (err, file) {
         file.format; // extracted formatting data. change at will.
 
         /* OUTPUT: RTree Data */
+        var file = "treeData.json";
         var treeData = tree.toJSON();
-        fs.writeFile("treeData.json", treeData, function(err) {
+        var jf = require('jsonfile');
+
+        jf.writeFileSync(file, treeData);
+        /*
+        fs.writeFile(file, treeData, function(err) {
                 if(err){
                             console.log(err);
                 }else{
                             console.log("The file was saved!");
                     }
         });
+        */
 });
 
 
 
 // TEST RTREE OUTPUT: 
-
+/*
 var file="treeData.json";
 var path = require('path'); // in node-core
 var filename = path.join(process.cwd(), file);
@@ -64,4 +70,4 @@ jsonFile(filename, function (err, file) {
         var result= tree.search(p);
         console.log("Result", result);
 });
-
+*/
